dcfe4b123dadcb76d1933e6e3bc88099485bb93e
Abdelrahman Ehab <abdu.ehab0@gmail.com>
2022-10-12 22:15:23 +0200

    fixed namespaces

--- include/Profiling/Instrumentor.hpp
+++ include/Profiling/Instrumentor.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../Constants.hpp"
+
 #if PROFILING
 
 #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
